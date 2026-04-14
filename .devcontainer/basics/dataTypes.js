de8d208297f532fb4f92997a7c285be4e64107a2
Nidhi Kedia <nidhikedia2204@gmail.com>
2026-04-14 11:28:05 +0000

    add initial dataTypes and variable files with basic examples

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as new version..
+
+//alert(3+3)  //we are using node.js not brower that it will show popup so here error
+
+console.log(3
+ +2) //this is wrong code readabilty should be high
+
+console.log("Nidhi")
+let name="Nidhi";
+let age=33;
+
+//number => 2 to power53
+//string=>""
+//boolean 
+//null=> standalone value //temp =0 no so temperature is null
+//undefined=> value not defined variable already dec 
+//symbol=>unique
+
+/*null is a object type whereas undefined itself is a type */
+
+
+console.log(typeof age);
---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,11 @@
+const accountId=14355;
+let accEmail="nidhikedia2204"
+var accPass="12345";
+let accountCity;
+
+//accountId=2..  cannot as it is const
+
+
+/* never use var as it is old and also block scope issue and functional scope..
+*/ 
+console.table([accEmail,accountId,accPass,accountCity]);
