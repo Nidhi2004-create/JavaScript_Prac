d7f11435ba93dd7f22b56b39591b2cb865260add
Nidhi Kedia <nidhikedia2204@gmail.com>
2026-04-14 13:40:28 +0000

    string idea

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,7 @@
+console.log(`Hello nidhi${1+2}`)
+
+const gameName=new String("cricket");
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
