dfc55161b3366b767598c7948aec531d5bcd67dd
Nidhi Kedia <nidhikedia2204@gmail.com>
2026-04-14 11:44:19 +0000

    conversionOfDataTypes

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score=33;
+let d="334eds"
+let valueInNumber=Number(score);
+let num=Number(d);
+console.log(typeof valueInNumber);
+console.log(typeof num);
+console.log(num);
+
+    //notes:
+//"33"=>33
+//"334eds"=>NaN 
+//true=>1 
+//null =>0
+//undefined=>NaN
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+    //notes:
+//1=>true
+//0=>false
+//null=>false
+//undefined=>false
+//"string"=>true  
+// ""=>false  
